9c6ec6d1e24260cf44a794188f6c1ac644891cd2
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-03-27 13:27:24 -0400

    use strict

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@
 // Created on: March 2023
 // This file contains the JS functions for index.html
 
-"strict"
+"use strict";
 
 function calculateClicked() {
   //Input through Textfields
-  const triBase = parseInt(document.getElementById("triangle-base").value)
-  const triHeight = parseInt(document.getElementById("triangle-height").value)
+  const triBase = parseInt(document.getElementById("triangle-base").value);
+  const triHeight = parseInt(document.getElementById("triangle-height").value);
 
   //Output
   document.getElementById("answer").innerHTML =
-    "The area of this Triangle is: " + (triBase * triHeight) / 2 + " cm²"
+    "The area of this Triangle is: " + (triBase * triHeight) / 2 + " cm²";
 }
